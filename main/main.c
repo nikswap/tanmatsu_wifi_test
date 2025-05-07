87f63cd153fa5cdec629e83085b5597226534d11
Renze Nicolai <renze@rnplus.nl>
2025-05-07 18:10:27 +0200

    Initialize graphics stack based on BSP display information

--- main/main.c
+++ main/main.c
@@ -19,9 +19,10 @@ static char const TAG[] = "main";
 // Global variables
 static size_t                       display_h_res        = 0;
 static size_t                       display_v_res        = 0;
-static lcd_color_rgb_pixel_format_t display_color_format;
-static pax_buf_t                    fb                = {0};
-static QueueHandle_t                input_event_queue = NULL;
+static lcd_color_rgb_pixel_format_t display_color_format = LCD_COLOR_PIXEL_FORMAT_RGB565;
+static lcd_rgb_data_endian_t        display_data_endian  = LCD_RGB_DATA_ENDIAN_LITTLE;
+static pax_buf_t                    fb                   = {0};
+static QueueHandle_t                input_event_queue    = NULL;
 
 void blit(void) {
     bsp_display_blit(0, 0, display_h_res, display_v_res, pax_buf_get_pixels(&fb));
@@ -42,15 +43,49 @@ void app_main(void) {
     // Initialize the Board Support Package
     ESP_ERROR_CHECK(bsp_device_initialize());
 
-    res = bsp_display_get_parameters(&display_h_res, &display_v_res, &display_color_format);
+    // Get display parameters and rotation
+    res = bsp_display_get_parameters(&display_h_res, &display_v_res, &display_color_format, &display_data_endian);
     ESP_ERROR_CHECK(res);  // Check that the display parameters have been initialized
+    bsp_display_rotation_t display_rotation = bsp_display_get_default_rotation();
 
+    // Convert ESP-IDF color format into PAX buffer type
+    pax_buf_type_t format = PAX_BUF_24_888RGB;
+    switch (display_color_format) {
+        case LCD_COLOR_PIXEL_FORMAT_RGB565:
+            format = PAX_BUF_16_565RGB;
+            break;
+        case LCD_COLOR_PIXEL_FORMAT_RGB888:
+            format = PAX_BUF_24_888RGB;
+            break;
+        default:
+            break;
+    }
+
+    // Convert BSP display rotation format into PAX orientation type
+    pax_orientation_t      orientation      = PAX_O_UPRIGHT;
+    switch (display_rotation) {
+        case BSP_DISPLAY_ROTATION_90:
+            orientation = PAX_O_ROT_CCW;
+            break;
+        case BSP_DISPLAY_ROTATION_180:
+            orientation = PAX_O_ROT_HALF;
+            break;
+        case BSP_DISPLAY_ROTATION_270:
+            orientation = PAX_O_ROT_CW;
+            break;
+        case BSP_DISPLAY_ROTATION_0:
+        default:
+            orientation = PAX_O_UPRIGHT;
+            break;
+    }
+
+    // Initialize graphics stack
+    pax_buf_init(&fb, NULL, display_h_res, display_v_res, format);
+    pax_buf_reversed(&fb, display_data_endian == LCD_RGB_DATA_ENDIAN_BIG);
+    pax_buf_set_orientation(&fb, orientation);
+
+    // Get input event queue from BSP
     ESP_ERROR_CHECK(bsp_input_get_queue(&input_event_queue));
-
-    // Initialize the graphics stack
-    pax_buf_init(&fb, NULL, display_h_res, display_v_res, PAX_BUF_16_565RGB);
-    pax_buf_reversed(&fb, false);
-    pax_buf_set_orientation(&fb, PAX_O_ROT_CW);
 
     ESP_LOGW(TAG, "Hello world!");
 
